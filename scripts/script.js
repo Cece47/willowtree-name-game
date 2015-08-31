abb439e50b11949eaa0ff10e17b7cbac9c902368
Colleen Carroll <leenmcarroll@gmail.com>
2015-08-31 18:39:18 -0400

    Testing

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
-//fade title out
-$('#main-header').delay(4000).fadeOut(500);
-//fade game title in
-$("#name").delay(4000).fadeIn(500);
+
+$(function() {
+
+  'use strict'
+
+    //fade title out
+    $('#main-header').delay(4000).fadeOut(500);
+    //fade game title in
+    $("#name").delay(4000).fadeIn(500);
+
+//selecting name
+
+  
+
+});
